dec44125fa5ad2eb02d04d225da79187e3a2e27c
poming\pomingpo <roy989898@gmail.com>
2017-05-26 19:43:27 +0800

    base project create

---
+++ src/main/java/com/example/demo/ToyforshotsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyforshotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyforshotsApplication.class, args);
+	}
+}
