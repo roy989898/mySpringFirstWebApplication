e6744d6deabc45e8f8b2dfb95feb97f62ad5530e
poming\pomingpo <roy989898@gmail.com>
2017-05-27 08:52:13 +0800

    remove the requestmapping /

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PageController {
 
-    @RequestMapping("/")
+ /*   @RequestMapping("/")
     public String home() {
         return "Hello";
-    }
+    }*/
 }
