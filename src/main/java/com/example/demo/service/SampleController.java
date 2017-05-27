47b3275476ef75df2950d8272c0bfc8094112bd0
poming\pomingpo <roy989898@gmail.com>
2017-05-27 20:24:44 +0800

    use jtwig so the list

--- src/main/java/com/example/demo/service/SampleController.java
+++ src/main/java/com/example/demo/service/SampleController.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
+import com.google.common.collect.Lists;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by pomingpo on 2017/5/27.
@@ -16,7 +19,8 @@ public class SampleController {
 
     @RequestMapping("/{name}")
     public String indexAction(ModelMap model, @PathVariable("name") String name) {
-        model.addAttribute("name", name);
+        List<String> list = Lists.newArrayList("Jan", "Peter", "Jane");
+        model.addAttribute("letterlist", list);
         return "index";
     }
 }
